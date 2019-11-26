b2967065b7e8382028a4d798930ccc907079defb
n8wb <vanish172@gmail.com>
2019-11-26 06:57:14 -0800

    create the interface contract

--- definition.go
+++ definition.go
@@ -1,39 +1,42 @@
+/*
+	Copyright 2019 whiteblock Inc.
+	This file is a part of the Definition.
+
+	Definition is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Definition is distributed in the hope that it will be useful,
+	but dock ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 package definition
 
-import (
-	"encoding/json"
-
-	"github.com/whiteblock/testexecution/pkg/definition/schema"
-	"github.com/whiteblock/testexecution/pkg/definition/validator"
+import(
+	"github.com/whiteblock/definition/command"
+	"github.com/whiteblock/definition/command/biome"
 )
 
-// Definition is the top level container for the test definition
-// specification
-type Definition struct {
-	ID string
+//Definition is the representation of the test definition format. 
+type Definition interface {
+	//Gets the UUID which uniquely identifies this Definition
+	GetID() string
 
-	spec schema.RootSchema
-}
+	//GetCommands gets the commands in dependency groups, so that
+	//res[n+1] is the set of commands which require the execution of the commands
+	//in res[n].
+	GetCommands() ([][]command.Command,error)
 
-func NewDefintion() string {
-	return ""
-}
+	//GetProvisioningRequest calculates the biome resources necessary to support
+	//this test
+	GetProvisioningRequest() (biome.CreateBiome, error)
 
-func (d *Definition) Valid() (interface{}, error) {
-	v, err := validator.NewValidator()
-	if err != nil {
-		return nil, err
-	}
-
-	data, err := json.Marshal(d.spec)
-	if err != nil {
-		return nil, err
-	}
-
-	err = v.Validate(data)
-	if err != nil {
-		return v.Errors(), err
-	}
-
-	return nil, nil
+	//Validate returns nil if it is a valid test definition, other the returned 
+	//error will contain an explanation for the issue
+	Validate() error
 }
---
+++ internal/definition.go
@@ -0,0 +1,38 @@
+package definition
+
+import (
+	"encoding/json"
+
+	"github.com/whiteblock/testexecution/pkg/definition/schema"
+	"github.com/whiteblock/testexecution/pkg/definition/validator"
+)
+
+// Definition is the top level container for the test definition
+// specification
+type Definition struct {
+	ID string
+	spec schema.RootSchema
+}
+
+func NewDefintion() string {
+	return ""
+}
+
+func (d *Definition) Valid() (interface{}, error) {
+	v, err := validator.NewValidator()
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := json.Marshal(d.spec)
+	if err != nil {
+		return nil, err
+	}
+
+	err = v.Validate(data)
+	if err != nil {
+		return v.Errors(), err
+	}
+
+	return nil, nil
+}
