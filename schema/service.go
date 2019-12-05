c6ca5fc6e38b9c1675fdb23db16257b7972df0d8
Ramin Keene <raminkeene@gmail.com>
2019-12-05 13:02:44 -0800

    temp fix

--- schema/service.go
+++ schema/service.go
@@ -4,10 +4,10 @@ type Service struct {
 	Name          string            `yaml:"name,omitempty" json:"name,omitempty"`
 	Description   string            `yaml:"description,omitempty" json:"description,omitempty"`
 	SharedVolumes []SharedVolume    `yaml:"shared-volumes,omitempty" json:"shared-volumes,omitempty"`
-	Resources     Resources         `yaml:"resources,omitempty" json:"resources,omitempty"`
+	Resources     *Resources        `yaml:"resources,omitempty" json:"resources,omitempty"`
 	Args          []string          `yaml:"args,omitempty" json:"args,omitempty"`
 	Environment   map[string]string `yaml:"environment,omitempty" json:"environment,omitempty"`
 	Image         string            `yaml:"image,omitempty" json:"image,omitempty"`
-	Script        Script            `yaml:"script,omitempty" json:"script,omitempty"`
+	Script        *Script           `yaml:"script,omitempty" json:"script,omitempty"`
 	InputFiles    []InputFile       `yaml:"input-files,omitempty" json:"input-files,omitempty"`
 }
