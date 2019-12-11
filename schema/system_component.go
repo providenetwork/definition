ab66a7428703250db8499050f51ac3cad9c1cbbf
n8wb <vanish172@gmail.com>
2019-12-11 13:44:03 -0800

    add to schema the port mappings

--- schema/system_component.go
+++ schema/system_component.go
@@ -16,11 +16,12 @@ type SystemComponentResources struct {
 }
 
 type SystemComponent struct {
-	Type        string                   `yaml:"type,omitempty" json:"type,omitempty"`
-	Name        string                   `yaml:"name,omitempty" json:"name,omitempty"`
-	Count       int64                    `yaml:"count,omitempty" json:"count,omitempty"`
-	Resources   SystemComponentResources `yaml:"resources,omitempty" json:"resources,omitempty"`
-	Sidecars    []SystemComponentSidecar `yaml:"sidecars,omitempty" json:"sidecars,omitempty"`
-	Args        []string                 `yaml:"args,omitempty" json:"args,omitempty"`
-	Environment map[string]string        `yaml:"environment,omitempty" json:"environment,omitempty"`
+	Type         string                   `yaml:"type,omitempty" json:"type,omitempty"`
+	Name         string                   `yaml:"name,omitempty" json:"name,omitempty"`
+	Count        int64                    `yaml:"count,omitempty" json:"count,omitempty"`
+	Resources    SystemComponentResources `yaml:"resources,omitempty" json:"resources,omitempty"`
+	PortMappings []string                 `yaml:"port-mappings,omitempty" json:"portMappings,omitempty"`
+	Sidecars     []SystemComponentSidecar `yaml:"sidecars,omitempty" json:"sidecars,omitempty"`
+	Args         []string                 `yaml:"args,omitempty" json:"args,omitempty"`
+	Environment  map[string]string        `yaml:"environment,omitempty" json:"environment,omitempty"`
 }
