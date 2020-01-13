27451c707e581e5d11b82ee87d2ae1b82aef676d
n8wb <vanish172@gmail.com>
2020-01-12 18:13:55 -0800

    make life easier

--- parser.go
+++ parser.go
@@ -23,79 +23,35 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-var globalParser = NewParser()
-
-// Parser represents a parser of the test definition format
-type Parser interface {
-	// SchemaYAML takes a raw set of YAML bytes and
-	// de-serializes them to a Definition structure from just the test spec.
-	SchemaYAML(raw []byte) (Definition, error)
-
-	// YAML takes a raw set of YAML bytes and
-	// de-serializes them to a Definition structure.
-	YAML(raw []byte) (Definition, error)
-
-	// SchemaJSON takes a raw set of JSON bytes and
-	// de-serializes them to a Definition structure from just the test spec.
-	SchemaJSON(raw []byte) (Definition, error)
-
-	// JSON takes a raw set of JSON bytes and
-	// de-serializes them to a Definition structure.
-	JSON(raw []byte) (Definition, error)
-}
-
-type parser struct {
-}
-
-// NewParser creates a new parser
-func NewParser() Parser {
-	return &parser{}
-}
-
 // SchemaYAML takes a raw set of YAML bytes and
 // de-serializes them to a Definition structure from just the test spec.
-func (p parser) SchemaYAML(raw []byte) (out Definition, err error) {
+func SchemaYAML(raw []byte) (out Definition, err error) {
 	return out, yaml.UnmarshalStrict(raw, &out.Spec)
 }
 
 // YAML takes a raw set of YAML bytes and
 // de-serializes them to a Definition structure.
-func (p parser) YAML(raw []byte) (out Definition, err error) {
+func YAML(raw []byte) (out Definition, err error) {
 	return out, yaml.UnmarshalStrict(raw, &out)
 }
 
 // SchemaJSON takes a raw set of JSON bytes and
 // de-serializes them to a Definition structure from just the test spec.
-func (p parser) SchemaJSON(raw []byte) (out Definition, err error) {
+func SchemaJSON(raw []byte) (out Definition, err error) {
 	return out, json.Unmarshal(raw, &out.Spec)
 }
 
+// SchemaANY will first try yaml and then try JSON
+func SchemaANY(raw []byte) (Definition, error) {
+	def, err := SchemaYAML(raw)
+	if err != nil {
+		return SchemaJSON(raw)
+	}
+	return def, nil
+}
+
 // JSON takes a raw set of JSON bytes and
 // de-serializes them to a Definition structure.
-func (p parser) JSON(raw []byte) (out Definition, err error) {
+func JSON(raw []byte) (out Definition, err error) {
 	return out, json.Unmarshal(raw, &out)
-}
-
-// SchemaYAML takes a raw set of bytes and
-// de-serializes them to a Definition structure from just the test spec.
-func SchemaYAML(raw []byte) (Definition, error) {
-	return globalParser.SchemaYAML(raw)
-}
-
-// YAML takes a raw set of YAML bytes and
-// de-serializes them to a Definition structure.
-func YAML(raw []byte) (Definition, error) {
-	return globalParser.YAML(raw)
-}
-
-// SchemaJSON takes a raw set of JSON bytes and
-// de-serializes them to a Definition structure from just the test spec.
-func SchemaJSON(raw []byte) (Definition, error) {
-	return globalParser.SchemaJSON(raw)
-}
-
-// JSON takes a raw set of JSON bytes and
-// de-serializes them to a Definition structure.
-func JSON(raw []byte) (Definition, error) {
-	return globalParser.JSON(raw)
 }
